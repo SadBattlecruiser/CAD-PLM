5f699c58cc8ea68fbc162323d525ceea2d8f4917
Rashid Abidokov <N23R35@gmail.com>
2020-11-04 15:20:24 +0300

    Пока линии и точки сохраняем листами

--- src/flagsclass.py
+++ src/flagsclass.py
@@ -7,9 +7,15 @@ class FlagsClass():
     def __init__(self):
         self.draw_points = True             # Отрисовывать ли точки
         self.draw_lines = True              # Отрисовывать ли линии
+        self.reset_state()
+        print('FlagsClass constructor')
+
+    # Выставить флаги происходящих сейчас событий в дефолтные
+    def reset_state(self):
         self.in_line_draw = False           # Рисуем ли линию прямо сейчас?
         self.line_first_point = False       # Если рисуем, первая ли это точка?
-        print('FlagsClass constructor')
+        self.in_point_pos = False           # Задаем ли ограничения на положения точки?
+        self.in_select_point = False        # Выбираем ли сейчас точку?
 
     def change_draw_points(self):
         if self.draw_points:
@@ -26,10 +32,17 @@ class FlagsClass():
         print('draw_lines ', self.draw_lines)
 
     def change_in_line_draw(self):
-        if self.in_line_draw:
-            self.in_line_draw = False
-            self.line_first_point = False
-        else:
+        temp = self.in_line_draw
+        self.reset_state()
+        if not temp:
             self.in_line_draw = True
             self.line_first_point = True
         print('in_line_draw ', self.in_line_draw)
+
+    def change_in_point_pos(self):
+        temp = self.in_point_pos
+        self.reset_state()
+        if not temp:
+            self.in_point_pos = True
+            self.in_select_point = True
+        print('in_point_pos ', self.in_point_pos)
--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton, QApplication)
 from PyQt5.QtGui import *
-#from PyQt5.QtCore import Qt, QCoreApplication
 from PyQt5.QtCore import *
 from flagsclass import *
 #import numpy as np
@@ -13,21 +12,23 @@ from flagsclass import *
 class MyApp(QWidget):
     def __init__(self):
         self.flags = FlagsClass()       # Хранилище флагов состояний
-        self.points = []                # Лист точек
-        self.lines = []                 # Лист линий
+        self.points = []                # Лист обычных точек
+        self.lines = []                 # Лист обычных линий
+        self.selected_points = []       # Лист выбранных точек
+        self.selected_lines = []        # Лист выбранных линий
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        wow_btn = QPushButton('WOW', self)
-        wow_btn.clicked.connect(lambda: self.printButton('test'))
-        wow_btn.resize(wow_btn.sizeHint())
-        wow_btn.move(50, 50)
-        #
-        line_btn = QPushButton('Line', self)
+        line_btn = QPushButton('Line draw', self)
         line_btn.clicked.connect(lambda: self.lineButton())
         line_btn.resize(line_btn.sizeHint())
         line_btn.move(0, 0)
+        #
+        point_pos_btn = QPushButton('Point pos', self)
+        point_pos_btn.clicked.connect(lambda: self.pointPosButton())
+        point_pos_btn.resize(point_pos_btn.sizeHint())
+        point_pos_btn.move(0, 30)
         #
         self.setGeometry(300, 300, 500, 500)
         self.setWindowTitle('lab1 CAD')
@@ -42,15 +43,21 @@ class MyApp(QWidget):
     def mousePressEvent(self, event):
         print(event)
         print('X:', event.x(), 'Y:', event.y())
+        f = self.flags
         # Если сейчас рисуем линию
-        if self.flags.in_line_draw:
-            if self.flags.line_first_point:
+        if f.in_line_draw:
+            if f.line_first_point:
                 self.first_point = event.pos()
-                self.flags.line_first_point = False
+                f.line_first_point = False
             else:
                 self.second_point = event.pos()
                 self.lines.append(QLineF(self.first_point, self.second_point))
-                self.flags.line_first_point = True
+                f.line_first_point = True
+        # Если сейчас выбираем точку
+        elif f.in_select_point:
+            #self.selected_points.append(self.findClosePoint(event.pos()))
+            self.selected_points.append(event.pos())
+            pass
         # Иначе просто ставим точку
         else:
             self.points.append(event.pos())
@@ -74,6 +81,18 @@ class MyApp(QWidget):
             painter.setPen(pen)
             for line_i in self.lines:
                 painter.drawLine(line_i)
+        # Отрисовка выбранных точек и линий
+        pen.setWidth(6)
+        pen.setColor(QColor(100,150,50))
+        painter.setPen(pen)
+        for point_i in self.selected_points:
+            painter.drawPoint(point_i)
+        pen.setWidth(3)
+        painter.setPen(pen)
+        for line_i in self.selected_lines:
+            painter.drawLine(line_i)
+        pen.setColor(QColor(0,0,0))
+        painter.setPen(pen)
         # Отрисовка первой точки линии, если она уже задана
         if self.flags.in_line_draw and not self.flags.line_first_point:
             pen.setWidth(6)
@@ -86,23 +105,20 @@ class MyApp(QWidget):
 
 
     ### Обработчики
-    def printButton(self, name):
-        print(name)
-
     def lineButton(self):
+        self.selected_points = []
+        self.selected_lines = []
         self.flags.change_in_line_draw()
-    #def drawPoint(self, pos, color=Qt.black):
-    #    painter = QPainter(self)
-    #    pen = QPen(color, 3)
-    #    painter.setPen(pen)
-    #    painter.drawPoint(pos)
+        self.update()
 
-    #def drawLine(self, color='black'):
-    #    painter = QPainter(self)
-    #    pen = QPen(Qt.red, 3)
-    #    painter.setPen(pen)
-    #    painter.drawLine(10, 10, self.rect().width() -10 , 10)
+    def pointPosButton(self):
+        self.selected_points = []
+        self.selected_lines = []
+        self.flags.change_in_point_pos()
+        self.update()
 
+    def findClosePoint(self):
+        pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
