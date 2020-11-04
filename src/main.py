bf1a44f7dd6cc3f6bcc4472960b67fceaec1d2e0
Rashid Abidokov <N23R35@gmail.com>
2020-11-04 16:13:15 +0300

    Точки задаются массивом

--- src/main.py
+++ src/main.py
@@ -6,18 +6,20 @@ from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton, QApplication)
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from flagsclass import *
-#import numpy as np
+
+import numpy as np
 
 
 class MyApp(QWidget):
     def __init__(self):
-        self.flags = FlagsClass()       # Хранилище флагов состояний
-        self.points = []                # Лист обычных точек
-        self.lines = []                 # Лист обычных линий
-        self.selected_points = []       # Лист выбранных точек
-        self.selected_lines = []        # Лист выбранных линий
+        self.flags = FlagsClass()                       # Хранилище флагов состояний
+        self.points = np.empty([0, 2])                  # Лист обычных точек
+        self.lines = []                                 # Лист обычных линий
+        self.selected_points = np.empty([0, 2])         # Лист выбранных точек
+        self.selected_lines = []                        # Лист выбранных линий
         super().__init__()
         self.initUI()
+        print('MyApp constructor')
 
     def initUI(self):
         line_btn = QPushButton('Line draw', self)
@@ -29,6 +31,11 @@ class MyApp(QWidget):
         point_pos_btn.clicked.connect(lambda: self.pointPosButton())
         point_pos_btn.resize(point_pos_btn.sizeHint())
         point_pos_btn.move(0, 30)
+        #
+        point_pos_btn = QPushButton('test', self)
+        point_pos_btn.clicked.connect(lambda: self.testButton())
+        point_pos_btn.resize(point_pos_btn.sizeHint())
+        point_pos_btn.move(0, 60)
         #
         self.setGeometry(300, 300, 500, 500)
         self.setWindowTitle('lab1 CAD')
@@ -56,11 +63,12 @@ class MyApp(QWidget):
         # Если сейчас выбираем точку
         elif f.in_select_point:
             #self.selected_points.append(self.findClosePoint(event.pos()))
-            self.selected_points.append(event.pos())
-            pass
+            print(self.selected_points)
+            self.selected_points = np.vstack([self.selected_points, [event.x(), event.y()]])
         # Иначе просто ставим точку
         else:
-            self.points.append(event.pos())
+            #self.points.append(event.pos())
+            self.points = np.vstack([self.points, [event.x(), event.y()]])
         self.update()
 
     # Отвечает за отрисовку всего
@@ -74,7 +82,7 @@ class MyApp(QWidget):
             pen.setWidth(6)
             painter.setPen(pen)
             for point_i in self.points:
-                painter.drawPoint(point_i)
+                painter.drawPoint(point_i[0], point_i[1])
         # Отрисовка обычных линий
         if self.flags.draw_lines:
             pen.setWidth(3)
@@ -86,7 +94,7 @@ class MyApp(QWidget):
         pen.setColor(QColor(100,150,50))
         painter.setPen(pen)
         for point_i in self.selected_points:
-            painter.drawPoint(point_i)
+            painter.drawPoint(point_i[0], point_i[1])
         pen.setWidth(3)
         painter.setPen(pen)
         for line_i in self.selected_lines:
@@ -106,16 +114,21 @@ class MyApp(QWidget):
 
     ### Обработчики
     def lineButton(self):
-        self.selected_points = []
+        self.selected_points = np.empty([0, 2]) 
         self.selected_lines = []
         self.flags.change_in_line_draw()
         self.update()
 
     def pointPosButton(self):
-        self.selected_points = []
+        self.selected_points = np.empty([0, 2])
         self.selected_lines = []
         self.flags.change_in_point_pos()
         self.update()
+
+    def testButton(self):
+        print(self.points)
+        self.update()
+
 
     def findClosePoint(self):
         pass
