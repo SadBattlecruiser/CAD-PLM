282b16d1c53f97944da9d6109df9c6729d5672dd
Rashid Abidokov <N23R35@gmail.com>
2021-01-10 20:40:08 +0300

    Начата плм 2

---
+++ PLM-2/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = GuiClass()
+    sys.exit(app.exec_())
